78d8266d6ec696e709863e0910080e62ca1ab4d4
Juuso Haavisto <juuso@mail.com>
2014-05-21 22:30:04 +0300

    routes: add Update and Remove to posts

--- main.go
+++ main.go
@@ -41,6 +41,9 @@ func main() {
 			res.HTML(200, "post/new", nil)
 		})
 		r.Get("/:title", ReadPost)
+		r.Get("/:title/edit", EditPost)
+		r.Get("/:title/delete", DeletePost)
+		r.Post("/:title/edit", strict.ContentType("application/x-www-form-urlencoded"), binding.Form(Post{}), binding.ErrorHandler, UpdatePost)
 		r.Post("/new", strict.ContentType("application/x-www-form-urlencoded"), binding.Form(Post{}), binding.ErrorHandler, CreatePost)
 
 	})
@@ -80,6 +83,8 @@ func main() {
 
 		r.Get("/posts", ReadPosts)
 		r.Get("/post/:title", ReadPost)
+		r.Post("/post/:title/edit", strict.ContentType("application/json"), binding.Json(Post{}), binding.ErrorHandler, UpdatePost)
+		r.Get("/post/:title/delete", DeletePost)
 		r.Post("/post", strict.ContentType("application/json"), binding.Json(Post{}), binding.ErrorHandler, CreatePost)
 
 	})
--- posts.go
+++ posts.go
@@ -92,8 +92,83 @@ func ReadPost(req *http.Request, params martini.Params, res render.Render, db *r
 	res.JSON(500, map[string]interface{}{"error": "Internal server error"})
 }
 
-// Inserts Post object to database. The function fetches user data from session and
-// after that it fills Author, Date and Excerpt fields.
+func EditPost(req *http.Request, params martini.Params, res render.Render, db *r.Session) {
+	var post Post
+	if params["title"] == "new" {
+		res.JSON(406, map[string]interface{}{"error": "You cant name a post with colliding route name!"})
+		return
+	}
+	post.Slug = params["title"]
+	post, err := post.Get(db)
+	if err != nil {
+		res.JSON(500, map[string]interface{}{"error": "Internal server error"})
+		log.Println(err)
+		return
+	}
+	switch root(req) {
+	case "api":
+		res.JSON(200, post)
+		return
+	case "post":
+		res.HTML(200, "post/edit", post)
+		return
+	}
+	res.JSON(500, map[string]interface{}{"error": "Internal server error"})
+}
+
+func UpdatePost(req *http.Request, params martini.Params, s sessions.Session, res render.Render, db *r.Session, post Post) {
+	post.Slug = params["title"]
+	entry, err := post.Get(db)
+	if err != nil {
+		res.JSON(500, map[string]interface{}{"error": "Internal server error"})
+		log.Println(err)
+		return
+	}
+	post, err = entry.Update(db, s, post)
+	if err != nil {
+		res.JSON(500, map[string]interface{}{"error": "Internal server error"})
+		log.Println(err)
+		return
+	}
+	switch root(req) {
+	case "api":
+		res.JSON(200, post)
+		return
+	case "post":
+		res.Redirect("/post/"+post.Slug, 302)
+		return
+	}
+	res.JSON(500, map[string]interface{}{"error": "Internal server error"})
+}
+
+func DeletePost(req *http.Request, params martini.Params, s sessions.Session, res render.Render, db *r.Session) {
+	var post Post
+	post.Slug = params["title"]
+	post, err := post.Get(db)
+	if err != nil {
+		res.JSON(500, map[string]interface{}{"error": "Internal server error"})
+		log.Println(err)
+		return
+	}
+	err = post.Delete(db, s)
+	if err != nil {
+		res.JSON(500, map[string]interface{}{"error": "Internal server error"})
+		log.Println(err)
+		return
+	}
+	switch root(req) {
+	case "api":
+		res.JSON(200, map[string]interface{}{"success": "Post deleted"})
+		return
+	case "post":
+		res.Redirect("/user", 302)
+		return
+	}
+	res.JSON(500, map[string]interface{}{"error": "Internal server error"})
+}
+
+// Insert inserts Post object into database. The function fetches user data from session and
+// after that it fills Author, Date and Excerpt fields. Returns created Post object.
 func (post Post) Insert(db *r.Session, s sessions.Session) (Post, error) {
 	var person Person
 	person, err := person.Session(db, s)
@@ -130,6 +205,60 @@ func (post Post) Get(s *r.Session) (Post, error) {
 		return post, err
 	}
 	return post, err
+}
+
+// Update updates a `entry` paramter with `post` parameter. Returns updated Post object.
+func (entry Post) Update(db *r.Session, s sessions.Session, post Post) (Post, error) {
+	var person Person
+	person, err := person.Session(db, s)
+	if err != nil {
+		return post, err
+	}
+	if entry.Author == person.Id {
+		row, err := r.Table("posts").Filter(func(this r.RqlTerm) r.RqlTerm {
+			return this.Field("slug").Eq(entry.Slug)
+		}).Update(map[string]interface{}{"content": post.Content, "slug": slug.Make(post.Title), "title": post.Title, "excerpt": Excerpt(post.Content)}).RunRow(db)
+		if err != nil {
+			return post, err
+		}
+		if row.IsNil() {
+			return post, errors.New("Nothing was found.")
+		}
+		err = row.Scan(&post)
+		if err != nil {
+			return post, err
+		}
+	} else {
+		return post, errors.New("Unauthorized.")
+	}
+	return post, err
+}
+
+// Delete deletes a post.
+func (post Post) Delete(db *r.Session, s sessions.Session) error {
+	var person Person
+	person, err := person.Session(db, s)
+	if err != nil {
+		return err
+	}
+	if post.Author == person.Id {
+		row, err := r.Table("posts").Filter(func(this r.RqlTerm) r.RqlTerm {
+			return this.Field("slug").Eq(post.Slug)
+		}).Delete().RunRow(db)
+		if err != nil {
+			return err
+		}
+		if row.IsNil() {
+			return errors.New("Nothing was found.")
+		}
+		err = row.Scan(&post)
+		if err != nil {
+			return err
+		}
+	} else {
+		return errors.New("Unauthorized.")
+	}
+	return err
 }
 
 func (post Post) GetAll(s *r.Session) ([]Post, error) {
