f308eabbe01298e24d8f5868366dc779f1c3ad5b
Jay Boehm <jayrox@gmail.com>
2014-12-04 19:55:43 -0500

    updated feeds.go to prevent exposure of unpublished items
    updated feeds.go to prevent exposure of unpublished items

--- feeds.go
+++ feeds.go
@@ -48,9 +48,13 @@ func ReadFeed(w http.ResponseWriter, res render.Render, db *gorm.DB, r *http.Req
 			return
 		}
 
+		// Don't expose unpublished items to the feeds
+		if !post.Published {
+			continue
+		}
+
 		// The email in &feeds.Author is not actually exported, as it is left out by user.Get().
 		// However, the package panics if too few values are exported, so that will do.
-	
 		item := &feeds.Item{
 			Title:       post.Title,
 			Link:        &feeds.Link{Href: urlhost + post.Slug},
