51330f1c391bca8d0a6a3814ab5bf940e83cd391
Juuso Haavisto <juuso@mail.com>
2015-07-07 12:42:58 +0300

    add forgotten absolute import path

--- databases/gorm/users.go
+++ databases/gorm/users.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 
 	. "github.com/9uuso/vertigo/crypto"
-	. "vertigo/email"
+	. "github.com/9uuso/vertigo/email"
 
 	"code.google.com/p/go-uuid/uuid"
 	"github.com/jinzhu/gorm"
