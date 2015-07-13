8df742eb06107bdda2f3fea65e9cc80a2fa14481
Juuso Haavisto <juuso@mail.com>
2015-07-13 11:45:52 +0300

    user multiline comment on settings.go example

--- settings/settings.go
+++ settings/settings.go
@@ -34,42 +34,50 @@ type SMTP struct {
 	Hostname string `json:"hostname" form:"smtp-hostname"`
 }
 
-// Settings is the global variable which holds settings stored in the settings.json file.
-// After importing the package, you can manipulate the variable using the Settings keyword.
-// Although, as mentioned in the Vertigo struct, be careful when dealing with the Firstrun and CookieHash values.
-// Rewriting Firstrun to true will render installation wizard on homepage, letting anyone redeclare your settings.
-// CookieHash on the other hand will return the secret hash used to sign your cookies, which might result in accounts getting compromised.
-//
-//  package mypackage
-//  
-//  import "github.com/9uuso/vertigo/settings"
-//  
-//  func Foobar() {
-//    fmt.Println(Settings.Name)
-//    // Output: Foobar's Blog
-//    settings = *Settings
-//    settings.Name = "Juuso's Blog"
-//    settings.Save()
-//    if err != nil {
-// 	    panic(err)
-//    }
-//    fmt.Println(Settings.Name)
-//    // Output: Juuso's Blog
-//  }
-//
-//  func ReadSettings() {
-// 	  var safesettings Vertigo
-// 	  safesettings = *Settings
-// 	  safesettings.CookieHash = "" // CookieHash wont be exposed
-// 	  switch Root(req) {
-// 	  case "api":
-//      render.JSON(200, safesettings)
-//      return
-//    case "user":
-//      render.HTML(200, "settings", safesettings)
-//      return
-//    }
-//  }
+/*
+
+Settings is the global variable which holds settings stored in the settings.json file.
+After importing the package, you can manipulate the variable using the Settings keyword.
+Although, as mentioned in the Vertigo struct, be careful when dealing with the Firstrun and CookieHash values.
+Rewriting Firstrun to true will render installation wizard on homepage, letting anyone redeclare your settings.
+CookieHash on the other hand will return the secret hash used to sign your cookies, which might result in accounts getting compromised.
+
+	package mypackage
+
+	import (
+		"fmt"
+
+		"github.com/9uuso/vertigo/settings"
+	)
+
+	func Foobar() {
+		fmt.Println(Settings.Name)
+		// Output: Foobar's Blog
+		settings = *Settings
+		settings.Name = "Juuso's Blog"
+		settings.Save()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(Settings.Name)
+		// Output: Juuso's Blog
+	}
+
+	func ReadSettings() {
+		var safesettings Vertigo
+		safesettings = *Settings
+		safesettings.CookieHash = "" // CookieHash wont be exposed
+		switch Root(req) {
+		case "api":
+			render.JSON(200, safesettings)
+			return
+		case "user":
+			render.HTML(200, "settings", safesettings)
+			return
+		}
+	}
+	
+*/
 var Settings = VertigoSettings()
 
 // VertigoSettings populates the global namespace with data from settings.json.
