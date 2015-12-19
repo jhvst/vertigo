2fdd6fa62c81d6ac1c19e6d9feb5d5254768cd66
Juuso Haavisto <juuso@mail.com>
2015-12-19 18:12:43 +0200

    shortdate is now displayed in Roboto Monospace, shortdate format updated to DD MMM YYYY

--- render/render.go
+++ render/render.go
@@ -56,7 +56,7 @@ var helpers = template.FuncMap{
 		return time.Unix(d, 0).UTC().In(time.FixedZone("", offset)).Format("Monday, January 2, 2006 3:04PM (-0700 GMT)")
 	},
 	"shortdate": func(d int64, offset int) string {
-		return time.Unix(d, 0).UTC().In(time.FixedZone("", offset)).Format("Jan 02 2006")
+		return time.Unix(d, 0).UTC().In(time.FixedZone("", offset)).Format("02 Jan 2006")
 	},
 	// env returns environment variable of s.
 	"env": func(s string) string {
