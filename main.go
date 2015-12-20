b6b50b12cd11620a289abc12b84ea5effb98669e
Juuso Haavisto <juuso@mail.com>
2015-12-20 15:21:26 +0200

    handle PORT environment variable now that martini is removed

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -366,5 +369,14 @@ func NewServer() *vestigo.Router {
 
 func main() {
 	server := NewServer()
-	http.ListenAndServe(":3000", server)
+	if os.Getenv("PORT") == "" {
+		log.Fatal(http.ListenAndServe(":3000", server))
+	} else {
+		port, err := strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			log.Println("Port was defined but could not be parsed.")
+			os.Exit(1)
+		}
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server))
+	}
 }
